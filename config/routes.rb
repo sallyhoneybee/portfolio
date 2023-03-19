43e9ddd491850d9952a683a52e43248ed9abf0ea
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-19 19:37:57 +0800

    Created pages home root path, projects model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'projects/index'
+  get 'projects/show'
 
   # Defines the root path route ("/")
   # root "articles#index"
